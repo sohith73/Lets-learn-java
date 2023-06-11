7f33c3d89e4c25d9fe85598fffcf569bb581a5e7
Sohith Bandreddi <98731252+sohith73@users.noreply.github.com>
2023-06-11 14:28:18 +0530

    adding user input feature

--- reverse.java
+++ reverse.java
@@ -1,6 +1,8 @@
+import java.util.scanner;
 class Reverse{
      public static void main(String[] args) {
-          int num = 89088;
+          Scanner sc = new Scanner(System.in);
+          int num = sc.nextInt();
           while (num>0){
                System.out.print(num%10);
                num /= 10;
